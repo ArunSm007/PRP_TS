63390af30fea847ca9c3d51e00ade26950bead4f
ArunSm007 <teamzzappy@gmail.com>
2017-03-13 12:17:08 +0530

    Venue

--- src/java/org/synergy/prp_ts/administrators/VenueAdmin.java
+++ src/java/org/synergy/prp_ts/administrators/VenueAdmin.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.synergy.prp_ts.administrators;
 
 import org.synergy.prp_ts.DAO.VenueDao;
 import org.synergy.prp_ts.beans.VenueDetails;
 
-/**
- *
- * @author Arun
- */
 public class VenueAdmin {
     
     public static int addVenue(String venueName){
